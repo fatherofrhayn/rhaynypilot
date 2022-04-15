fac47109a4d232407fe14ec49b21cff8adc01c8f
fatherofrhayn <100546262+fatherofrhayn@users.noreply.github.com>
2022-04-15 14:39:44 -0400

    Revert "UI: new laneless design (#24169)"
    This reverts commit b40c378d0f2f9672a34da12e507d349b7b7270c8.

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -201,8 +201,8 @@ protected:
   void initializeGL() override;
   void showEvent(QShowEvent *event) override;
   void updateFrameMat(int w, int h) override;
-  void drawLaneLines(QPainter &painter, const UIState *s);
-  void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd);
+  void drawLaneLines(QPainter &painter, const UIScene &scene);
+  void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd, bool openpilot_longitudinal_control);
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
   inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
   inline QColor greenColor(int alpha = 255) { return QColor(49, 201, 34, alpha); }
